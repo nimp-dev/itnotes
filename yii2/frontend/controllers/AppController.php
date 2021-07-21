c21ec1bc8d45e4d9439291a426fc007573345a78
egor <egorkonopka93@gmail.com>
2021-07-21 12:49:24 +0300

    start

---
+++ yii2/frontend/controllers/AppController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ *
+ */
+class AppController extends Controller
+{
+//    protected function setMeta($title = null, $keywords = null, $description = null){
+//        $this->view->title = $title;
+//        $this->view->registerMetaTag(['name' => 'keywords', 'content' => "$keywords"]);
+//        $this->view->registerMetaTag(['name' => 'description', 'content' => "$description"]);
+//    }
+
+
+}
